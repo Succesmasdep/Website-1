ea4fb771d64aab6dffe132b967c938ac88c70ebc
Ngapa <krilinamar@gmail.com>
2021-03-20 10:47:56 +0700

    update

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 
@@ -10,3 +11,7 @@ class Post(models.Model):
 
     def __str__(self):
         return self.judul
+
+    def get_absolute_url(self):
+        return reverse("detail-artikel", kwargs={"pk": self.pk})
+    
