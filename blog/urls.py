e3c337d2d80343cc47dcf674a2c6beae06e24a12
Ngapa <krilinamar@gmail.com>
2021-03-26 21:20:51 +0700

    update

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,9 @@ from .views import HomeView, ArtikelDetail, AddPostView, PostUpdateView, PostDel
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('article/<int:pk>', ArtikelDetail.as_view(), name='detail-artikel'),
-    path('add-post/', AddPostView.as_view(), name='post-artikel'),
+    path('add_post/', AddPostView.as_view(), name='post-artikel'),
     path('artikel/edit/<int:pk>', PostUpdateView.as_view(), name='edit-artikel'),
     path('artikel/<int:pk>/remove', PostDeleteView.as_view(), name='hapus-artikel'),
-    path('add_category/', AddCategoryView.as_view(), name='add=category'),
+    path('add_category/', AddCategoryView.as_view(), name='add-category'),
     path('category/<str:cats>/', CategoryView, name='kategori'),
     ]
