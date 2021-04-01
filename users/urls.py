4e5a9400ef0df5bdb7b3c6e017af1802ab3c5522
Ngapa <krilinamar@gmail.com>
2021-04-01 19:08:28 +0700

    Update

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import UserRegisterView, UserEditView, PasswordChangeView
+from .views import UserRegisterView, UserEditView, PasswordChangeView, ShowProfilePageView, EditProfilePageView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', UserRegisterView.as_view(), name='register'),
     path('edit_profile/', UserEditView.as_view(), name='edit-profile'),
     path('password/', PasswordChangeView.as_view()),
+    path('<int:pk>/profile/', ShowProfilePageView.as_view(), name= 'profile-page'),
+    path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(), name= 'edit-profile-page'),
     ]
